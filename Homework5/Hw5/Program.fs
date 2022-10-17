7f321189f6b89397feb6f5763b16095be043460d
Kuchina Olga <olmenua2003@gmail.com>
2022-10-17 11:42:45 +0300

    Кучина 2.1.5

--- Homework5/Hw5/Program.fs
+++ Homework5/Hw5/Program.fs
@@ -1,3 +1,19 @@
 ﻿open System
+open Hw5
 
-(NotImplementedException() |> raise)       
+let createMessage message =
+    match message with
+        | Message.WrongArgLength -> "You have to pass 3 arguments"
+        | Message.WrongArgFormat -> "Argument could not be converted to the double type"
+        | Message.WrongArgFormatOperation -> "Unknown operation"
+        | Message.DivideByZero -> "Divide by zero"
+        
+let printResult message status =
+    printfn $"{message}"
+    (int)status
+    
+[<EntryPoint>]
+let Main args =
+    match Parser.parseCalcArguments args with
+        | Ok (arg1,operation,arg2) -> printResult (Calculator.calculate arg1 operation arg2) Message.SuccessfulExecution 
+        | Error message -> printResult (createMessage message) message
