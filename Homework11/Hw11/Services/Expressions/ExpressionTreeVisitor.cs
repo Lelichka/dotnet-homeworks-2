daa29c0f70e0a53b9cd7a1de9586de00652138cd
Kuchina Olga <olmenua2003@gmail.com>
2022-11-29 19:26:59 +0300

    Кучина 2.1.11

---
+++ Homework11/Hw11/Services/Expressions/ExpressionTreeVisitor.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq.Expressions;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using Hw11.ErrorMessages;
+
+namespace Hw11.Services.ExpressionTree;
+
+public class ExpressionTreeVisitor
+{
+    public static async Task<double> VisitExpression(Expression expr)
+    {
+        return await Visit((dynamic)expr);
+    }
+    async public static Task<double> Visit(BinaryExpression expression)
+    {
+        await Task.Delay(1000);
+        var leftExpr = Task.Run(() => VisitExpression(expression.Left));
+        var rightExpr = Task.Run(() => VisitExpression(expression.Right));
+        var res = await Task.WhenAll(leftExpr, rightExpr);
+
+        var constLeft = res[0];
+        var constRight = res[1];
+
+        return Calculate(expression.NodeType, constLeft, constRight);
+            
+    }
+    async public static Task<double> Visit(ConstantExpression expression)
+    {
+        return (double)expression.Value;
+    }
+
+    public static double Calculate(ExpressionType binExpr, double constLeft,double constRight)
+    {
+        return (binExpr) switch
+        {
+            ExpressionType.Add => constLeft + constRight,
+            ExpressionType.Subtract => constLeft - constRight,
+            ExpressionType.Multiply => constLeft * constRight,
+            ExpressionType.Divide => constRight == 0.0
+                ? throw new Exception(MathErrorMessager.DivisionByZero)
+                : constLeft / constRight,
+            _ => throw new Exception(MathErrorMessager.UnknownCharacter)
+        };
+    } 
+}
--- Homework11/Hw11/Services/MathCalculator/MathCalculatorService.cs
+++ Homework11/Hw11/Services/MathCalculator/MathCalculatorService.cs
@@ -1,9 +1,16 @@
+using Hw11.Dto;
+using Hw11.Services.ExpressionTree;
+using Hw9.Services.Expressions;
+
 namespace Hw11.Services.MathCalculator;
 
 public class MathCalculatorService : IMathCalculatorService
 {
     public async Task<double> CalculateMathExpressionAsync(string? expression)
     {
-        throw new NotImplementedException();
+        Validator.Validate(expression);
+        var parseResult = Parser.ConvertToPostfixForm(expression);
+        var exprTree = ExpressionTreeBuilder.CreateExpressionTree(parseResult);
+        return await ExpressionTreeVisitor.VisitExpression((dynamic)exprTree);
     }
 }
